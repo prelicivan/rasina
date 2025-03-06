6d75b71e324d0442e8aee2780c00a3fa29ff581b
Ivan Prelic <ivanprelicp@gmail.com>
2025-03-06 16:38:21 +0100

    minor changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 import { FaBars, FaTimes } from 'react-icons/fa'
 import './Navbar.css'
 import { IconContext } from "react-icons/lib";
-import logo from "/pictures/RAG_logo-Osnovni-Vector.svg"; 
+import logo from "/pictures/RAG logo-Horizontalni-Vector.svg"; 
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -33,7 +33,7 @@ function Navbar() {
     <IconContext.Provider value={{ color: '#022552'}}>
       <div className="navbar">
         <div className="navbar-container container">
-          <Link to="hero" className="navbar-logo" onClick={closeMobileMenu} spy={true} smooth={true} duration={500}>
+          <Link to="hero" className="navbar-logo" onClick={closeMobileMenu} spy={true} smooth={true} duration={600}>
             <img src={logo} className="navbar-img" width="200" height="100" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -41,12 +41,12 @@ function Navbar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
-              <Link to="about" className="nav-links" spy={true} smooth={true} offset={-70} duration={500} onClick={closeMobileMenu}>
+              <Link to="about" className="nav-links" spy={true} smooth={true} offset={-70} duration={600} onClick={closeMobileMenu}>
                 O nama
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="contact" className="nav-links" spy={true} smooth={true} offset={-50} duration={500} onClick={closeMobileMenu}>
+              <Link to="contact" className="nav-links" spy={true} smooth={true} offset={-50} duration={600} onClick={closeMobileMenu}>
                 Kontakt
               </Link>
             </li>
