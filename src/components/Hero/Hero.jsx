8dc51f854f42fcacc4c1c47386613f07911c62f4
Ivan Prelic <ivanprelicp@gmail.com>
2025-03-10 13:40:33 +0100

    minor changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero({
 }) {
     return(
         <div className={flip ? 'hero-section-flip' : 'hero-section'} id="hero">
-            <motion.div initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0, transition: { duration: 1.5 } }} viewport={{ once: true}} >
+            <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { duration: 1.5 } }} viewport={{ once: true}} >
                 <img src="/pictures/RAG logo-Osnovni-Medium.png" className="hero-logo"></img>
             </motion.div>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
 import { Link } from "react-scroll";
 import { FaBars, FaTimes } from 'react-icons/fa'
 import './Navbar.css'
